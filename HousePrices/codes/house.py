14c5fb0c49db6f7fd8a46ab63a7a124b9e51f4b8
xinghouliu <xinghouliu@datamesh.com>
2019-06-19 19:16:11 +0800

    0.9999

--- HousePrices/codes/house.py
+++ HousePrices/codes/house.py
@@ -1,21 +1,26 @@
-import pandas as pd
-import numpy as np
-import xgboost as xgb
-from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
-from sklearn.model_selection import GridSearchCV, KFold
-from sklearn.externals import joblib
+import pandas as pd
+import xgboost as xgb
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import Lasso
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.preprocessing import Imputer
+
 
 def split_data(data):
     y = data['SalePrice']
     X = data.drop(['SalePrice'], axis=1)
     X = pd.get_dummies(X)
-    print("X shape:{}, y shape{}".format(X.shape, y.shape))
-    return X, y
+    test_data = X.iloc[1460:]
+    X = X.iloc[:1460]
+    y = y.iloc[:1460]
+    return X, y, test_data
 
 def get_data():
     train_data = pd.read_csv("../datasets/train.csv")
-    print(train_data.shape)
+    test_data = pd.read_csv("../datasets/test.csv")
+    train_data = pd.concat([train_data, test_data], axis=0, sort=True)
     return train_data
 
 def get_test_data():
@@ -31,16 +36,17 @@ def visualization(data):
 
 def train_model(X, y):
     param_gv = {
-        'subsample': [1]
+        'n_estimators': [1000, 500, 800]
     }
     bst = xgb.XGBRegressor(
         max_depth=22,
         min_child_weight=3,
         eta=0.5,
-        n_estimators=1000,
+        # n_estimators=1000,
         learning_rate=0.1,
         gamma=0,
         subsample=1,
+        eval_metric='rmse',
         objective='reg:squarederror',
         nthread=12,
         colsample_bytree=0.75,
@@ -52,25 +58,176 @@ def train_model(X, y):
     print("regression result:\n score: {}\t, best_params:{} \t ,best_score:{}".format(model.scorer_, model.best_params_, model.best_score_))
     return model
 
-def score_model(X, y):
+def all_data_model(X, y):
+    X = nan_imputer(X)
     bst = xgb.XGBRegressor(
         max_depth=22,
         min_child_weight=3,
         eta=0.5,
-        n_estimators=1000,
+        n_estimators=800,
         learning_rate=0.1,
         gamma=0,
         subsample=1,
+        objective='reg:squarederror',
+        eval_metric='rmse',
+        nthread=12,
+        colsample_bytree=0.75,
+        colsample_bylevel=0.7,
+        seed=100
+    )
+    bst.fit(X, y)
+    selection = SelectFromModel(bst, prefit=True)
+    X_new = selection.transform(X)
+    model = xgb.XGBRegressor(
+        max_depth=15,
+        min_child_weight=3,
+        eta=0.5,
+        n_estimators=110,
+        learning_rate=0.1,
+        gamma=0,
+        subsample=1,
+        eval_metric='rmse',
+        objective='reg:squarederror',
+        nthread=12,
+        colsample_bytree=0.75,
+        colsample_bylevel=0.5,
+        seed=100
+    )
+    model.fit(X_new, y)
+    y_pred = model.predict(X_new)
+    print("r2 score:{}".format(r2_score(y, y_pred)))
+    print("X new:{}".format(X_new[:2]))
+    print("X new:{}\t type x :{} ".format(X_new.shape, type(X_new)))
+
+def selection_model_cv(X, y):
+    bst = xgb.Booster(model_file="all_data_model.model")
+    selection = SelectFromModel(bst, prefit=True)
+    X_new = selection.transform(X)
+    # cv train
+    param_gv = {
+        'n_estimators': [100, 200, 300],
+    }
+    bst = xgb.XGBRegressor(
+        max_depth=15,
+        min_child_weight=3,
+        eta=0.5,
+        # n_estimators=100,
+        learning_rate=0.1,
+        gamma=0,
+        subsample=1,
+        eval_metric='rmse',
         objective='reg:squarederror',
         nthread=12,
         colsample_bytree=0.75,
         colsample_bylevel=0.7,
         seed=100
     )
-    bst.fit(X, y, eval_metric='rmse')
-    joblib.dump(bst, "train_model.m")
-    # train_pred = bst.predict(X)
-    # print("r2_score : {}".format(r2_score(y, train_pred)))
+    model = GridSearchCV(estimator=bst, param_grid=param_gv, scoring='r2', cv=10)
+    model.fit(X_new, y)
+    print("regression result:\n score: {}\t, best_params:{} \t ,best_score:{}".format(model.scorer_, model.best_params_,
+                                                                                      model.best_score_))
+def selection_model(X, y):
+    X = nan_imputer(X)
+    bst_old = xgb.Booster(model_file="all_data_model.model")
+    selection = SelectFromModel(bst_old, prefit=True)
+    X_new = selection.transform(X)
+    model = xgb.XGBRegressor(
+        max_depth=15,
+        min_child_weight=3,
+        eta=0.5,
+        n_estimators=110,
+        learning_rate=0.1,
+        gamma=0,
+        subsample=1,
+        eval_metric='rmse',
+        objective='reg:squarederror',
+        nthread=12,
+        colsample_bytree=0.75,
+        colsample_bylevel=0.5,
+        seed=100
+    )
+    model.fit(X_new, y)
+    y_pred = model.predict(X_new)
+    print("r2 score:{}".format(r2_score(y, y_pred)))
+    return X_new.columns
+
+def score_model(X, y):
+    X = nan_imputer(X)
+    bst = xgb.XGBRegressor(
+        max_depth=22,
+        min_child_weight=3,
+        eta=0.5,
+        n_estimators=800,
+        learning_rate=0.1,
+        gamma=0,
+        subsample=1,
+        objective='reg:squarederror',
+        eval_metric='rmse',
+        nthread=12,
+        colsample_bytree=0.75,
+        colsample_bylevel=0.7,
+        seed=100
+    )
+    bst.fit(X, y)
+    # selection model
+    selection = SelectFromModel(bst, prefit=True)
+    X_new = selection.transform(X)
+
+    # do grid search
+    param_gv = {
+        'subsample': [0.95, 0.98, 1],
+        # 'colsample_bylevel': [0.4, 0.5, 0.6]
+        # 'n_estimators': [100, 150, 200]
+    }
+    bst = xgb.XGBRegressor(
+        max_depth=15,
+        min_child_weight=3,
+        eta=0.5,
+        n_estimators=110,
+        learning_rate=0.1,
+        gamma=0,
+        subsample=1,
+        eval_metric='rmse',
+        objective='reg:squarederror',
+        nthread=12,
+        colsample_bytree=0.75,
+        colsample_bylevel=0.5,
+        seed=100
+    )
+    model = GridSearchCV(estimator=bst, param_grid=param_gv, scoring='r2', cv=10)
+    model.fit(X_new, y)
+    print("regression result:\n score: {}\t, best_params:{} \t ,best_score:{}".format(model.scorer_, model.best_params_,
+                                                                                      model.best_score_))
+    # bst_new = xgb.XGBRegressor(
+    #     max_depth=22,
+    #     min_child_weight=3,
+    #     eta=0.5,
+    #     n_estimators=800,
+    #     learning_rate=0.1,
+    #     gamma=0,
+    #     subsample=1,
+    #     objective='reg:squarederror',
+    #     eval_metric='rmse',
+    #     nthread=12,
+    #     colsample_bytree=0.75,
+    #     colsample_bylevel=0.7,
+    #     seed=100
+    # )
+    # X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size=0.2, random_state=100)
+    # bst_new.fit(X_train, y_train)
+    # y_pred = bst_new.predict(X_test)
+    # print("r2 score: {}".format(r2_score(y_test, y_pred)))
+
+    # print("x new shape:{}".format(X_new.shape))
+    # bst.save_model("train_model.model")
+    # # plot feature importance
+    # plot_importance(bst)
+    # plt.show()
+    # # features = X.columns
+    # # plt.bar(bst.f, bst.feature_importances_)
+    # # plt.show()
+    # y_pred = bst.predict(X)
+    # print("r2 score: {}".format(r2_score(y, y_pred)))
     # return bst
 
 def get_result(model, test_data):
@@ -78,11 +235,100 @@ def get_result(model, test_data):
     result = pd.DataFrame([test_data['Id'], y_pred], columns=['Id', 'SalePrice'])
     result.to_csv(path_or_buf='../datasets/submission.csv')
 
+def train_save_model():
+    train_data = get_data()
+    X, y, test_data = split_data(train_data)
+    score_model(X, y)
+
+def get_result():
+    train_data = get_data()
+    X, y, test_data = split_data(train_data)
+    # bst = score_model(X, y)
+    bst = xgb.Booster(model_file="train_model.model")
+    test_xgb_data = xgb.DMatrix(test_data)
+    test_pred = bst.predict(test_xgb_data)
+    test_series_pred = pd.Series(test_pred, name='SalePrice')
+    result = pd.DataFrame()
+    result['Id'] = test_data['Id']
+    result['SalePrice'] = test_series_pred
+    print("result head:{}".format(result.head))
+    result.to_csv(path_or_buf='submission.csv', index=False)
+
+'''
+垃圾
+'''
+def lasso_regression(X, y):
+    X = nan_imputer(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
+    model = Lasso()
+    model.fit(X_train, y_train)
+    print('系数矩阵:{}\n'.format(model.coef_))
+    print('线性回归模型:{}\n'.format(model))
+    y_pred = model.predict(X_test)
+    accuracy = r2_score(y_test, y_pred)
+    print("accuracy :{}".format(accuracy))
+
+def train_test_split_model(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
+    X_train = nan_imputer(X_train)
+    X_test = nan_imputer(X_test)
+    bst = xgb.XGBRegressor(
+        max_depth=22,
+        min_child_weight=3,
+        eta=0.5,
+        n_estimators=100,
+        learning_rate=0.1,
+        gamma=0,
+        subsample=1,
+        objective='reg:squarederror',
+        eval_metric='rmse',
+        nthread=12,
+        colsample_bytree=0.75,
+        colsample_bylevel=0.7,
+        seed=100
+    )
+    bst.fit(X_train, y_train)
+    y_pred = bst.predict(X_test)
+    accuracy = r2_score(y_test, y_pred)
+    print("Accurancy: %.2f%%" % (accuracy * 100.0))
+    thresholds = sorted(bst.feature_importances_)
+    print(thresholds)
+    for thresh in thresholds:
+        selection = SelectFromModel(bst, threshold=thresh, prefit=True)
+        select_x_train = selection.transform(X_train)
+        selection_model = xgb.XGBRegressor(
+            max_depth=22,
+            min_child_weight=3,
+            eta=0.5,
+            n_estimators=100,
+            learning_rate=0.1,
+            gamma=0,
+            subsample=1,
+            objective='reg:squarederror',
+            eval_metric='rmse',
+            nthread=12,
+            colsample_bytree=0.75,
+            colsample_bylevel=0.7,
+            seed=100
+        )
+        selection_model.fit(select_x_train, y_train)
+        select_x_test = selection.transform(X_test)
+        y_pred = selection_model.predict(select_x_test)
+        accuracy = r2_score(y_test, y_pred)
+        print("Thresh=%.3f, n=%d, Accuracy: %.2f%%" % (thresh, select_x_train.shape[1], accuracy * 100.0))
+
+def nan_imputer(data):
+    imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    imputer.fit(data)
+    return imputer.transform(data)
+
 if __name__ == '__main__':
-    get_data()
-    get_test_data()
-    # train_data = get_data()
-    # X, y = split_data(train_data)
-    # model = score_model(X, y)
-    # get_result(joblib.load("train_model.m"), get_test_data())
+    train_data = get_data()
+    X, y, data = split_data(train_data)
+    all_data_model(X, y)
+    # score_model(X, y)
+    # train_test_split_model(X, y)
+    # train_model(X, y)
+    # train_save_model()
+    # get_result()
     # visualization(train_data)
---
+++ HousePrices/codes/kaggle_manito.py
@@ -0,0 +1,187 @@
+'''
+kaggle 大神代码  https://www.kaggle.com/abhinand05/predicting-housingprices-simple-approach-lb-top3
+'''
+
+import numpy as np
+import pandas as pd
+pd.set_option('display.width', 1000)
+from IPython.display import display, HTML
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+import seaborn as sns
+import missingno as msno  #search for missing value
+
+# Import Sci-Kit Learn
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Normalizer
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor, BaggingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV, cross_val_score, StratifiedKFold, learning_curve, KFold
+
+# Ensemble Models
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+
+# Package for stacking models
+from vecstack import stacking
+
+display(HTML("""
+<style>
+.output_png {
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+}
+</style>
+"""))
+
+def show_all(df):
+    with pd.option_context('display.max_rows', 100, 'display.max_columns', 10):
+        display(df)
+
+def import_data():
+    train_data = pd.read_csv("../datasets/train.csv", keep_default_na=False, index_col='Id')
+    test_data = pd.read_csv("../datasets/test.csv", keep_default_na=False, index_col='Id')
+    # train_data = pd.concat([train_data, test_data], axis=0, sort=True)
+    # train_data.info()  # 这个命令可以看出哪些列的数据不全。
+    # print(train_data.describe())
+    return train_data, test_data
+
+'''
+ intuitively understand what's missing in our data and where it is missing.
+'''
+def plot_missing(df):
+    missing = df.isnull().sum()
+    missing = missing[missing > 0]
+
+    # plot missing value
+    missing.plot.bar(figsize=(12, 8))
+    plt.xlabel('Columns with missing values')
+    plt.ylabel('Count')
+    plt.show()
+    msno.matrix(df=df, figsize=(16, 8), color=(0, 0.2, 1), labels=True)
+    plt.show()
+
+'''
+为数值列填充列的media值
+为分类列填充最常见的分类
+'''
+def fill_missing_data(df):
+    missing = df.isnull().sum()
+    missing = missing[missing > 0]  # 得到有缺失值的列
+    for column in list(missing.index):
+        if df[column].dtype == 'object':
+            df[column].fillna(df[column].value_counts().index[0], inplace=True)  # 分类列中最常见的分类
+        elif df[column].dtype == 'int64' or 'float64' or 'int16' or 'float16':
+            df[column].fillna(df[column].median(), inplace=True)
+
+'''
+文本类型的数据换成数字（离散）
+'''
+def impute_cats(df):
+    object_cols = list(df.select_dtypes(exclude=[np.number]).columns)
+    object_cols_ind = []
+    for i in object_cols:
+        object_cols_ind.append(df.columns.get_loc(i))
+    label_enc = LabelEncoder()
+    for i in object_cols_ind:
+        # print("i:{}\t name:{}".format(i, object_cols[i]))
+        df.iloc[:, i] = label_enc.fit_transform(df.iloc[:, i])
+
+def corr(df):
+    corr_mat = df[["SalePrice", "MSSubClass", "MSZoning", "LotFrontage", "LotArea", "BldgType",
+                      "OverallQual", "OverallCond", "YearBuilt", "BedroomAbvGr", "PoolArea", "GarageArea",
+                      "SaleType", "MoSold"]].corr()
+    # f, ax = plt.subplots(figsize=(20, 20))
+    # sns.heatmap(corr_mat, vmax=1, square=True)
+    # plt.show() # 发现 OverallQual YearBuilt GarageArea 和 出售价格相关性较大
+    # sns.lineplot(x='YearBuilt', y='SalePrice', data=df)
+    # plt.show()
+    # sns.lineplot(x='OverallQual', y='SalePrice', data=df)
+    # plt.show()
+    # sns.lineplot(x='GarageArea', y='SalePrice', data=df)
+    # plt.show()
+    # sns.distplot(df['SalePrice'])  # 销售价格的分布
+    # plt.show()
+    sns.catplot(x='SaleType', y='SalePrice', data=df, kind='bar', palette='muted')
+    plt.show()
+
+def integrated_models(train_data, test_data):
+    print("train data shape:{}, test data shape:{}".format(train_data.shape, test_data.shape))
+    y = train_data['SalePrice']
+    X = train_data.drop('SalePrice', axis=1)
+
+    kf = KFold(n_splits=5)
+    # random_forest
+    random_forest = RandomForestRegressor(n_estimators=800, max_depth=15, min_samples_leaf=5, max_features=None, random_state=10, oob_score=True)
+    y_pred = cross_val_score(random_forest, X, y, cv=kf, n_jobs=5)
+    print('random forest mean: {}'.format(y_pred.mean()))
+    random_forest.fit(X, y)
+    rf_pred = random_forest.predict(test_data)
+
+    # xgboost predict
+    xg_boost = XGBRegressor(learning_rate=0.01, n_estimators=6000, max_depth=4, min_child_weight=1, gamma=0, subsample=1, colsample_bytree=0.2, objective='reg:squarederror',
+                 eval_metric='rmse', nthread=-1, scale_pos_weight=1, seed=20, reg_alpha=0.00006)
+    y_pred = cross_val_score(xg_boost, X, y, cv=kf, n_jobs=-1)
+    print('xgboost mean:{}'.format(y_pred.mean()))
+    xg_boost.fit(X, y)
+    xgb_pred = xg_boost.predict(test_data)
+
+    # gradient boost regressor(GBM)
+    g_boost = GradientBoostingRegressor(n_estimators=6000, learning_rate=0.01, max_depth=5, max_features='sqrt', min_samples_leaf=15, min_samples_split=10, loss='ls', random_state=30)
+    y_pred = cross_val_score(g_boost, X, y, cv=kf, n_jobs=-1)
+    print('gbm mean:{}'.format(y_pred.mean()))
+    g_boost.fit(X, y)
+    gbm_pred = g_boost.predict(test_data)
+
+    # lightGBM regressor
+    lightgbm = LGBMRegressor(objective='regression', num_leaves=6,learning_rate=0.01,n_estimators=6400,verbose=-1,bagging_fraction=0.80,bagging_freq=4,bagging_seed=6,
+                             feature_fraction=0.2, feature_fraction_seed=7)
+    y_pred = cross_val_score(lightgbm, X, y, cv=kf, n_jobs=-1)
+    print('lgbm mean:{}'.format(y_pred.mean()))
+    lightgbm.fit(X, y)
+    lgb_pred = lightgbm.predict(test_data)
+
+    # model stacking
+    models = [g_boost, xg_boost, lightgbm, random_forest]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    S_train, S_test = stacking(models, X_train, y_train, X_test, regression=True, mode='oof_pred_bag', metric=rmse, n_folds=5, random_state=25, verbose=2)
+
+    # 初始化第二层模型
+    xgb_lev2 = XGBRegressor(learning_rate=0.1, n_estimators=500, max_depth=3, n_jobs=-1, random_state=17)
+    # Fit the 2nd level model on the output of level 1
+    xgb_lev2.fit(S_train, y_train)
+    stacked_pred = xgb_lev2.predict(S_test)
+    print("RMSE of Stacked Model: {}".format(rmse(y_test, stacked_pred)))
+
+    y1_pred_L1 = models[0].predict(test_data)
+    y2_pred_L1 = models[1].predict(test_data)
+    y3_pred_L1 = models[2].predict(test_data)
+    y4_pred_L1 = models[3].predict(test_data)
+    S_test_L1 = np.c_[rf_pred, xgb_pred, gbm_pred, lgb_pred]
+    # S_test_L1 = np.c_[y1_pred_L1, y2_pred_L1, y3_pred_L1, y4_pred_L1]
+    test_stacked_pred = xgb_lev2.predict(S_test_L1)
+    submission = pd.DataFrame()
+
+    submission['Id'] = np.array(test_data.index)
+    submission['SalePrice'] = test_stacked_pred
+    submission.to_csv("submission.csv", index=False)
+
+
+def rmse(y_true, y_pred):
+    return np.sqrt(mean_squared_error(np.log(y_true), np.log(y_pred)))
+
+if __name__ == '__main__':
+    train_data, test_data = import_data()
+    impute_cats(train_data)
+    impute_cats(test_data)
+    integrated_models(train_data, test_data)
+    # corr(train_data)
+
+    # print("train data shape:{}\t test data shape: {}".format(train_data.shape, test_data.shape))
+    # plot_missing(test_data)
+    # fill_missing_data(train_data)
+    # print(train_data.isnull().sum().max())
