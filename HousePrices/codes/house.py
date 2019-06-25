51299cc961cbcab4e338518b2bc2f3007f4aed05
steve <995270418@qq.com>
2019-06-25 23:28:16 +0800

    0.999999

--- HousePrices/codes/house.py
+++ HousePrices/codes/house.py
@@ -1,11 +1,18 @@
+import math
 import matplotlib.pyplot as plt
 import pandas as pd
 import xgboost as xgb
+from scipy.stats import skew
+from sklearn.cluster import KMeans
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import Lasso
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.preprocessing import Imputer
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler  # 数据预处理的标准化，(X - mean) / std 数据聚集在0 附近， 均值为1
+
 from scipy import stats
 import seaborn as sns
 import numpy as np
@@ -23,7 +30,7 @@ def get_data():
     train_data = pd.read_csv("../datasets/train.csv", index_col='Id')
     test_data = pd.read_csv("../datasets/test.csv", index_col='Id')
     # train_data = pd.concat([train_data, test_data], axis=0, sort=True)
-    return train_data
+    return train_data, test_data
 
 def get_test_data():
     data = pd.read_csv("../datasets/test.csv")
@@ -331,27 +338,39 @@ def explore_data_analysis(df=pd.DataFrame()):
     normality_test = lambda x: stats.shapiro(x.fillna(0))[1] < 0.01  # p_value 小于 0.01. 认为假设不成立，即不属于正态分布
     normal = quantitative.apply(normality_test)
     # print(normal.any()) # True 表示所有特征都不服从正态分布
+
     # 相关性 检测
+    features = spearn_corrlation(list(quantitative.columns), list(qualitative.columns), train_data)
+    print("features :{}".format(features))
+    # 降维数据并可视化， 查看聚类
+    # dimension_reduce_visualization(features, train_data)
+
+def dimension_reduce_visualization(features=[], train=pd.DataFrame()):
+    # 使用的是 tsne 方法（数据量小的时候用）
+    model = TSNE(n_components=2, perplexity=50, random_state=0)
+    X = train[features].fillna(0).values
+    tsne = model.fit_transform(X)
+    print("tsne shape: {}".format(tsne.shape))
+    std = StandardScaler()
+    s = std.fit_transform(X)
+    print("s shape:{}".format(s.shape))
+    pca = PCA(n_components=30)
+    pc = pca.fit_transform(s)   # pca 训练的数据都需要标准化
+    print("pc shape:{}".format(pc.shape))
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(pc)
+    fr = pd.DataFrame({"tsne1": tsne[:, 0], 'tsne2': tsne[:, 1], 'cluster': kmeans.labels_})
+    sns.lmplot(data = fr, x='tsne1', y='tsne2', hue='cluster', fit_reg=False)
+    plt.show()
+    print(np.sum(pca.explained_variance_ratio_))
+
+def spearn_corrlation(quantitative=[], qualitative=[], train_data=pd.DataFrame()):
     qual_encoded = []
-    for q in qualitative.columns:
+    for q in qualitative:
         encode(q, train_data)
         qual_encoded.append(q + "_E")
-    features = list(quantitative.columns) + qual_encoded
-    # print("features :{}".format(features))
-    # spearman(features, train_data)
-    plt.figure(1)
-    corr = train_data[list(quantitative.columns) + ['SalePrice']].corr()
-    sns.heatmap(corr, vmax=.8)
-    plt.figure(2)
-    corr = train_data[qual_encoded + ['SalePrice']].corr()
-    sns.heatmap(corr, vmax=.8)
-    plt.figure(3)
-    corr = pd.DataFrame(np.zeros([len(quantitative.columns) + 1, len(qual_encoded) + 1]), index=list(quantitative.columns) + ['SalePrice'], columns=qual_encoded + ['SalePrice'])
-    for q1 in list(quantitative.columns) + ['SalePrice']:
-        for q2 in qual_encoded + ['SalePrice']:
-            corr.loc[q1, q2] = train_data[q1].corr(train_data[q2])
-    sns.heatmap(corr)
-    plt.show()
+    features = quantitative + qual_encoded
+    return features
 
 def encode(feature, df=pd.DataFrame()):
     ordering = pd.DataFrame()
@@ -374,17 +393,58 @@ def spearman(features, df=pd.DataFrame()):
     sns.barplot(data=spr, y='feature', x='spearman', orient='h')
     plt.show()
 
-def feature_selection(df=pd.DataFrame()):
-    corr_mat = df.corr()
-    sns.heatmap(corr_mat, vmax=.8, square=True)
-    corr_mat.nla
-    print(corr_mat.inde)
-    plt.show()
-    #
+def feature_selection(train=pd.DataFrame(), test=pd.DataFrame()):
+    explore_data_analysis(train)
+    print("train data shape :{}".format(train.shape))
+    train = train[train['GrLivArea'] < 4500]
+    train['SalePrice'] = train['SalePrice'].map(lambda x : math.log(1+x))
+    y = train['SalePrice']
+    train_features = train.drop("SalePrice", axis=1)
+    test_features = test
+    features = pd.concat([train_features, test_features], sort=False)
+    features['MSSubClass'] = features['MSSubClass'].apply(str)
+    features['YrSold'] = features['YrSold'].astype(str)
+    features['MoSold'] = features['MoSold'].astype(str)
+    features['Functional'] = features['Functional'].fillna('Typ')
+    features['Electrical'] = features['Electrical'].fillna("SBrkr")
+    features['KitchenQual'] = features['KitchenQual'].fillna("TA")
+    features["PoolQC"] = features["PoolQC"].fillna("None")
+    features['Exterior1st'] = features['Exterior1st'].fillna(features['Exterior1st'].mode()[0])
+    features['Exterior2nd'] = features['Exterior2nd'].fillna(features['Exterior2nd'].mode()[0])
+    features['SaleType'] = features['SaleType'].fillna(features['SaleType'].mode()[0])
+
+    for col in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
+        features[col] = features[col].fillna(0)
+
+    for col in ['GarageType', 'GarageFinish', 'GarageQual', 'GarageCond']:
+        features[col] = features[col].fillna('None')
+
+    for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+        features[col] = features[col].fillna('None')
+
+    features['MSZoning'] = features.groupby('MSSubClass')['MSZoning'].transform(lambda x: x.fillna(x.mode()[0]))  # 填充出现频率最高的数(众数)
+
+    object = list(features.select_dtypes(include='object').columns)
+    features[object].fillna('None', inplace=True)
+    features['LotFrontage'] = features.groupby('Neighborhood')['LotFrontage'].transform(lambda x:x.fillna(x.median())) # 填充中位数
+    numeric_dtypes = list(features.select_dtypes(include=['int16', 'int32', 'int64', 'float16', 'float32', 'float64']).columns)
+    features[numeric_dtypes].fillna('None', inplace=True)
+
+    # 统计所有偏度 > 0.5 的features
+    skew_features = features[numeric_dtypes].apply(lambda x: skew(x)).sort_values(ascending=False) # 默认是从小到大排序， 这里不进行排序，节约时间
+
+    high_skew = skew_features[skew_features > 0.5]
+    skew_high_index = high_skew.index
+    print("high skew index: {}".format(skew_high_index))
+    for i in skew_high_index:
+        pass
+    # print(features.shape)
+    print(features.head())
 
 if __name__ == '__main__':
-    train_data = get_data()
-    explore_data_analysis(train_data)
+    train_data, test_data = get_data()
+    feature_selection(train_data, test_data)
+    # explore_data_analysis(train_data)
     # X, y, data = split_data(train_data)
     # all_data_model(X, y)
     # score_model(X, y)
